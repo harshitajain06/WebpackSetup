622495ffc798b8a0ff563ff94143bfe08c462322
harshitajain06 <harshitaforever06@gmail.com>
2022-12-08 18:28:23 +0530

    boilerplate changes

--- src/index.js
+++ src/index.js
@@ -4,14 +4,15 @@ import './style.css';
 import Icon from './icon.png';
 import Data from './data.xml';
 import Notes from './data.csv';
+
 function component() {
-    const element = document.createElement('div');
-    const btn = document.createElement('button');
-  
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-    // Add the image to our existing div.
+  const element = document.createElement('div');
+  const btn = document.createElement('button');
+
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+  // Add the image to our existing div.
   const myIcon = new Image();
   myIcon.src = Icon;
 
@@ -20,13 +21,12 @@ function component() {
   console.log(Data);
   console.log(Notes);
 
-
-    btn.innerHTML = 'Click me and check the console!';
+  btn.innerHTML = 'Click me and check the console!';
   btn.onclick = printMe;
 
   element.appendChild(btn);
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+
+  return element;
+}
+
+document.body.appendChild(component());
